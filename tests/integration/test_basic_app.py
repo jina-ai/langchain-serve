4040f733d8c39a2934f69c19edf8eb7fd7b68d81
Deepankar Mahapatro <deepankar.mahapatro@jina.ai>
2023-04-28 17:20:36 +0530

    feat: bearer token authorization

--- tests/integration/test_basic_app.py
+++ tests/integration/test_basic_app.py
@@ -6,6 +6,10 @@ import websockets
 
 from .helper import run_test_server
 
+HOST = 'localhost:8080'
+HTTP_HOST = f'http://{HOST}'
+WS_HOST = f'ws://{HOST}'
+
 
 @pytest.mark.parametrize(
     "run_test_server, route",
@@ -13,7 +17,7 @@ from .helper import run_test_server
     indirect=["run_test_server"],
 )
 def test_basic_app_http(run_test_server, route):
-    url = "http://localhost:8080/" + route
+    url = HTTP_HOST + route
     headers = {
         "accept": "application/json",
         "Content-Type": "application/json",
@@ -33,7 +37,90 @@ def test_basic_app_http(run_test_server, route):
     indirect=["run_test_server"],
 )
 async def test_basic_app_ws(run_test_server, route):
-    async with websockets.connect(f"ws://localhost:8080/{route}") as websocket:
+    async with websockets.connect(WS_HOST + route) as websocket:
+        await websocket.send(json.dumps({"interval": 1}))
+
+        received_messages = []
+        for _ in range(5):
+            message = await websocket.recv()
+            received_messages.append(message)
+
+        assert received_messages == ["0", "1", "2", "3", "4"]
+
+
+@pytest.mark.parametrize(
+    "run_test_server, route",
+    [("basic_app", "sync_auth_http")],
+    indirect=["run_test_server"],
+)
+def test_basic_app_http_authorized(run_test_server, route):
+    url = HTTP_HOST + route
+    data = {"interval": 1, "envs": {}}
+
+    # no auth headers
+    response = requests.post(url, json=data)
+    assert response.status_code == 403
+    assert response.json()["detail"] == "Not authenticated"
+
+    # not a bearer token
+    response = requests.post(url, headers={'Authorization': 'invalidtoken'}, json=data)
+    assert response.status_code == 403
+    assert response.json()["detail"] == "Not authenticated"
+
+    # invalid bearer token
+    response = requests.post(
+        url, headers={'Authorization': 'Bearer invalidtoken'}, json=data
+    )
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Invalid bearer token"
+
+    # valid bearer token
+    response = requests.post(
+        url, headers={'Authorization': 'Bearer mysecrettoken'}, json=data
+    )
+
+    assert response.status_code == 200
+    assert response.json()["result"] == "Hello, world!"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "run_test_server, route",
+    [("basic_app", "sync_auth_ws")],
+    indirect=["run_test_server"],
+)
+async def test_basic_app_ws_authorized(run_test_server, route):
+    url = WS_HOST + route
+
+    # no auth headers
+    with pytest.raises(websockets.InvalidStatusCode) as e:
+        async with websockets.connect(url) as websocket:
+            pass
+
+    assert e.value.status_code == 403
+
+    # not a bearer token
+    with pytest.raises(websockets.InvalidStatusCode) as e:
+        async with websockets.connect(
+            url, extra_headers={'Authorization': 'invalidtoken'}
+        ) as websocket:
+            pass
+
+    assert e.value.status_code == 403
+
+    # invalid bearer token
+    with pytest.raises(websockets.InvalidStatusCode) as e:
+        async with websockets.connect(
+            url, extra_headers={'Authorization': 'Bearer invalidtoken'}
+        ) as websocket:
+            pass
+
+    assert e.value.status_code == 403
+
+    # valid bearer token
+    async with websockets.connect(
+        url, extra_headers={'Authorization': 'Bearer mysecrettoken'}
+    ) as websocket:
         await websocket.send(json.dumps({"interval": 1}))
 
         received_messages = []
