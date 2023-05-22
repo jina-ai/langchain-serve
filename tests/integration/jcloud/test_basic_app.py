beabc5b8d030eb6e89986b4bbba5e35d91cc6c9b
Deepankar Mahapatro <deepankar.mahapatro@jina.ai>
2023-05-22 19:56:47 +0530

    test: assign and pass workspace to methods

--- tests/integration/jcloud/test_basic_app.py
+++ tests/integration/jcloud/test_basic_app.py
@@ -3,6 +3,7 @@ import json
 import pytest
 import requests
 import websockets
+from websockets.exceptions import ConnectionClosedOK
 
 from ..helper import deploy_jcloud_app
 
@@ -12,6 +13,7 @@ async def test_basic_app():
     async with deploy_jcloud_app() as app_id:
         _test_http_route(app_id)
         await _test_ws_route(app_id)
+        await _test_workspace(app_id)
 
 
 def _test_http_route(app_id):
@@ -41,3 +43,26 @@ async def _test_ws_route(app_id):
             received_messages.append(message)
 
         assert received_messages == ["0", "1", "2", "3", "4"]
+
+
+async def _test_workspace(app_id):
+    http_url = f"https://{app_id}.wolf.jina.ai/store"
+    ws_url = f"wss://{app_id}.wolf.jina.ai/stream"
+
+    for i in range(10):
+        data = {"text": f"Here's string {i}", "envs": {}}
+        response = requests.post(http_url, json=data)
+        assert response.status_code == 200
+
+    try:
+        async with websockets.connect(ws_url) as websocket:
+            await websocket.send(json.dumps({}))
+
+            received_messages = []
+            for _ in range(10):
+                message = await websocket.recv()
+                received_messages.append(message.strip())
+
+            assert received_messages == [f"Here's string {i}" for i in range(10)]
+    except ConnectionClosedOK:
+        pass
