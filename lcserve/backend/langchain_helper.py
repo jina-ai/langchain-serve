8f1892facf8ebf6801d4a77b06e7436310a0f8f8
Zac Li <zac.li@jina.ai>
2023-06-09 18:43:10 +0800

    feat: more events

--- lcserve/backend/langchain_helper.py
+++ lcserve/backend/langchain_helper.py
@@ -1,13 +1,17 @@
 import asyncio
+import json
+import logging
 from typing import Any, Dict, List, Optional
 from uuid import UUID
 
 from fastapi import WebSocket
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.schema import LLMResult
+from langchain.schema import AgentAction, LLMResult
 from opentelemetry.trace import Span, Tracer, set_span_in_context
 from pydantic import BaseModel, ValidationError
+
+logger = logging.getLogger(__name__)
 
 
 class TracingCallbackHandler(BaseCallbackHandler):
@@ -31,12 +35,18 @@ class TracingCallbackHandler(BaseCallbackHandler):
         prompts_len = 0
         operation = "langchain.on_llm_start"
 
-        context = set_span_in_context(self.parent_span)
-        with self.tracer.start_as_current_span("llm start", context=context) as span:
-            span.set_attribute("otel.operation.name", operation)
-            prompts_len += sum([len(prompt) for prompt in prompts])
-            span.set_attribute("num_processed_prompts", len(prompts))
-            span.set_attribute("prompts_len", prompts_len)
+        try:
+            context = set_span_in_context(self.parent_span)
+            with self.tracer.start_as_current_span(
+                "llm start", context=context
+            ) as span:
+                span.set_attribute("otel.operation.name", operation)
+                prompts_len += sum([len(prompt) for prompt in prompts])
+                span.set_attribute("num_processed_prompts", len(prompts))
+                span.set_attribute("prompts_len", prompts_len)
+                span.add_event("prompts", {"data": prompts})
+        except Exception:
+            logger.error('Error in LangChain callback handler', exc_info=True)
 
     def on_llm_end(self, response: LLMResult, *, run_id: UUID, **kwargs: Any) -> None:
         if not self.tracer:
@@ -45,11 +55,96 @@ class TracingCallbackHandler(BaseCallbackHandler):
         operation = "langchain.on_llm_end"
         token_usage = response.llm_output["token_usage"]
 
-        context = set_span_in_context(self.parent_span)
-        with self.tracer.start_as_current_span("llm end", context=context) as span:
-            span.set_attribute("otel.operation.name", operation)
-            for k, v in token_usage.items():
-                span.set_attribute(k, v)
+        try:
+            context = set_span_in_context(self.parent_span)
+            with self.tracer.start_as_current_span("llm end", context=context) as span:
+                span.set_attribute("otel.operation.name", operation)
+                for k, v in token_usage.items():
+                    span.set_attribute(k, v)
+
+                texts = "\n".join(
+                    [" ".join([l.text for l in lst]) for lst in response.generations]
+                )
+                span.add_event("outputs", {"data": texts})
+        except Exception:
+            logger.error('Error in LangChain callback handler', exc_info=True)
+
+    def on_chain_start(
+        self,
+        serialized: Dict[str, Any],
+        inputs: Dict[str, Any],
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        **kwargs: Any
+    ) -> None:
+        if not self.tracer:
+            return
+
+        operation = 'langchain.on_chain_start'
+
+        try:
+            context = set_span_in_context(self.parent_span)
+            with self.tracer.start_as_current_span(
+                "chain start", context=context
+            ) as span:
+                span.set_attribute("otel.operation.name", operation)
+                span.add_event("inputs", {"data": json.dumps(inputs)})
+        except Exception:
+            logger.error('Error in LangChain callback handler', exc_info=True)
+
+    def on_chain_end(
+        self,
+        outputs: Dict[str, Any],
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        **kwargs: Any
+    ) -> None:
+        if not self.tracer:
+            return
+
+        operation = 'langchain.on_chain_end'
+
+        try:
+            context = set_span_in_context(self.parent_span)
+            with self.tracer.start_as_current_span(
+                "chain end", context=context
+            ) as span:
+                span.set_attribute("otel.operation.name", operation)
+                span.add_event("outputs", {"data": json.dumps(outputs)})
+        except Exception:
+            logger.error('Error in LangChain callback handler', exc_info=True)
+
+    def on_agent_action(
+        self,
+        action: AgentAction,
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        **kwargs: Any
+    ) -> None:
+        if not self.tracer:
+            return
+
+        operation = 'langchain.on_agent_action'
+
+        try:
+            context = set_span_in_context(self.parent_span)
+            with self.tracer.start_as_current_span(
+                "agent action", context=context
+            ) as span:
+                span.set_attribute("otel.operation.name", operation)
+                span.add_event(
+                    "agent_action",
+                    {
+                        "data": action.tool,
+                        "tool_input": action.tool_input,
+                        "log": action.log,
+                    },
+                )
+        except Exception:
+            logger.error('Error in LangChain callback handler', exc_info=True)
 
 
 class AsyncStreamingWebsocketCallbackHandler(StreamingStdOutCallbackHandler):
